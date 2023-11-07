71c06e509e57cab8ddd7b59358bed9b7e2d50d23
Marlon <marlonvicente355@gmail.com>
2023-11-07 01:14:04 -0600

    mi proyecto

---
+++ SistemaVenta.AplicacionWeb/Controllers/subirarchivo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaVenta.AplicacionWeb.Controllers
+{
+    public class subirarchivo : Controller
+    {
+        [HttpPost]
+        public IActionResult SubirArchivo(IFormFile archivo)
+        {
+            if (archivo != null && archivo.Length > 0)
+            {
+                // Guarda el archivo en el servidor
+                var ruta = Path.Combine("C:/wwwroot/img//", archivo.FileName);
+                using (var archivoStream = new FileStream(ruta, FileMode.Create))
+                {
+                    archivo.CopyTo(archivoStream);
+                }
+                // Ahora 'ruta' contiene la ubicación del archivo en el servidor
+                return RedirectToAction("Negocio"); // O la acción que desees
+            }
+            return View("Error"); // Maneja el caso donde no se sube un archivo
+        }
+
+    }
+}
